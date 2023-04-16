b27a340061a91e7f1765f7456a04f5fea7368b29
Stainley Lebron <saiyamans@gmail.com>
2023-04-16 12:15:01 -0400

    sign-up saved into database

---
+++ js/signup.js
@@ -0,0 +1,33 @@
+function countCharacters() {
+    const message = document.getElementById("name");
+    const charCount = document.getElementById("charCount");
+    message.addEventListener("input", () => {
+        charCount.innerHTML = message.value.length;
+    });
+}
+
+
+function saveFormData(event) {
+    event.preventDefault();
+
+    // Get form data
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+        alert("Password is different!")
+        return;
+    }
+
+    // Save form to localStorage
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    alert("Form data saved successfully!");
+
+    event.target.reset();
+}
+
+countCharacters();
+saveFormData()
