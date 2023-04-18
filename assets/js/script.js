b133c5caea6d4df30d389246529c2e6dd66b559f
Stainley Lebron <stainley.lebron@gmail.com>
2023-04-18 16:39:49 -0400

    some features

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@ const main = () => {
 
 }
 
+const sendMail = () => {
+    document.getElementById("mail_to");
+    const email = "info@abit.com"
+    const mailto_link = 'mailto:' + email
+    window = window.open(mailto_link, 'emailWindow')
+   /* if (window && window.open && !window.closed){
+        window.close();
+    }*/
+}
+
 const copyright = () => {
     const year = new Date().getFullYear();
     document.getElementById("copyright").textContent = `© ${year} Abit. All Rights Reserved.`;
